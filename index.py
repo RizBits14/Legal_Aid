7a329b49ecfedab10211d300fe8db4e17ca1b891
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-04-11 00:50:54 +0600

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadTimeSignature
+from flask_mail import Mail, Message
+from flask import Flask, redirect, render_template, request, session, url_for, flash
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
+app.secret_key = 'Kage0012'
+s = URLSafeTimedSerializer(app.secret_key)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'dattebayo12086@gmail.com'
+app.config['MAIL_PASSWORD'] = 'qkmt eqqb fvvy oijr' #App Pass
 
-if __name__ == "__main__":
-    app.run(debug=True)
+mail = Mail(app)
+
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'legal_aid',
+    'port': '3306'
+}
+
+def create_connection():
+    return mysql.connector.connect(**db_config)
