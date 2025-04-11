289a4df475b7fc1f336d03a859bcbf49625948d2
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-04-12 00:16:29 +0600

    Update index.py

--- index.py
+++ index.py
@@ -28,3 +28,10 @@ db_config = {
 
 def create_connection():
     return mysql.connector.connect(**db_config)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
